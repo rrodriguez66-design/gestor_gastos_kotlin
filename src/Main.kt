b6c1948cfb2aefd31ed4db792e209a9b06c6bcc9
rrodriguez66-design <rrodriguez66@alumnos.uaq.mx>
2026-08-12 00:56:45 -0600

    Refactor calcularTotal to return total value
    Se hizo la corrección

--- src/Main.kt
+++ src/Main.kt
@@ -2,18 +2,12 @@ fun main() {
     println("Gestor semanal de gastos")
 }
 
-fun calcularTotal(montos: List<Double>) {
-    println("\n--- TOTAL DE GASTOS ---")
-
-    if (montos.isEmpty()) {
-        println("No hay gastos registrados para calcular.")
-    } else {
-        var total = 0.0
-        for (monto in montos) {
-            total += monto
-        }
-        println("El monto total gastado en la semana es: $$total")
+fun calcularTotal(montos: List<Double>): Double {
+    var total = 0.0
+    for (monto in montos) {
+        total += monto
     }
+    return total
 }
 
 
