d07f0ee19b0c7cbd07bf456e0b68e6aed61ff7b5
ivaldez16-UAQ <ivaldez16@alumnos.uaq.mx>
2026-08-11 23:46:21 -0600

    función para calcular el total de gastos

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
     println("Gestor semanal de gastos")
 }
+
+fun calcularTotal(montos: List<Double>) {
+    println("\n--- TOTAL DE GASTOS ---")
+
+    if (montos.isEmpty()) {
+        println("No hay gastos registrados para calcular.")
+    } else {
+        var total = 0.0
+        for (monto in montos) {
+            total += monto
+        }
+        println("El monto total gastado en la semana es: $$total")
+    }
+}
+
+
+
